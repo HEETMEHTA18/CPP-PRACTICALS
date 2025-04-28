4c8f0c93cd4270c99ed4eab3da94e4b3b645692b
Heet Mehta <heetmehta18125@gmail.com>
2025-04-28 15:30:35 +0530

    Updation Accroding to lab

--- PRACTICAL FILES/6.4/prac6_4.cpp
+++ PRACTICAL FILES/6.4/prac6_4.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
-
-class Base {
+using namespace std;
+class Base
+{
 public:
-    virtual ~Base() { std::cout << "Base destroyed\n"; }
+    Base () { cout << "Base created\n"; }
+    // Virtual destructor
+    virtual ~Base()
+    {
+        cout << "Base destroyed\n";
+    }
 };
 
-class Derived : public Base {
+class Derived : public Base
+{
     int *arr;
+
 public:
-    Derived() { arr = new int[5]; }
-    ~Derived() { delete[] arr; std::cout << "Derived destroyed\n"; }
+
+    Derived() { 
+        arr = new int[5];
+        cout << "Derived created\n";
+    }
+    ~Derived()
+    {
+        delete[] arr;
+        cout << "Derived destroyed\n";
+    }
 };
 
-int main() {
+int main()
+{
     Base *p = new Derived();
     delete p;
     return 0;
