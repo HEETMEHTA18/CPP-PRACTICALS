0bf2cfd5230551c70a7009061b1a08d41fe28aab
Heet Mehta <heetmehta18125@gmail.com>
2025-04-28 14:52:33 +0530

    Updated Changes

--- PRACTICAL FILES/6.3/prac6_3.cpp
+++ PRACTICAL FILES/6.3/prac6_3.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
+using namespace std;
 
-int main() {
+int main()
+{
     int n, m;
-    std::cin >> n >> m;
+    cin >> n >> m;
     int *a = new int[n];
     int *b = new int[m];
-    for (int i = 0; i < n; ++i) std::cin >> a[i];
-    for (int j = 0; j < m; ++j) std::cin >> b[j];
+    cout << "Enter the elements of the first array: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+    cout << "Enter the elements of the second array: ";
+    for (int j = 0; j < m; ++j)
+    {
+        cin >> b[j];
+    }
+
     int *c = new int[n + m];
+
     int i = 0, j = 0, k = 0;
+
     while (i < n && j < m)
+    {
         c[k++] = (a[i] < b[j]) ? a[i++] : b[j++];
-    while (i < n) c[k++] = a[i++];
-    while (j < m) c[k++] = b[j++];
-    for (int t = 0; t < k; ++t) std::cout << c[t] << ' ';
-    std::cout << std::endl;
+    }
+    while (i < n)
+    {
+        c[k++] = a[i++];
+    }
+    while (j < m)
+    {
+        c[k++] = b[j++];
+    }
+    cout << "Merged array: ";
+    for (int t = 0; t < k; ++t)
+    {
+        cout << c[t] << ' ';
+    }
+    cout << endl;
     delete[] a;
     delete[] b;
     delete[] c;
