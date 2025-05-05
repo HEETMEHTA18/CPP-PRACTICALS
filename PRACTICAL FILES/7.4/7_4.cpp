47ff7d9d20ee06bdb1925cc520a8f6784770746b
Heet Mehta <heetmehta18125@gmail.com>
2025-05-05 16:14:30 +0530

    Changes

--- PRACTICAL FILES/7.4/7_4.cpp
+++ PRACTICAL FILES/7.4/7_4.cpp
@@ -13,7 +13,11 @@ int main() {
     while (fin >> n >> m >> g)
         v.push_back({n,m,g});
     cout << left << setw(15) << "Name" << setw(7) << "Marks" << setw(7) << "Grade" << endl;
+    cout<<"-----------------------------------" << endl;
     for (auto &r : v)
+    {
+       
         cout << setw(15) << r.name << setw(7) << r.marks << setw(7) << r.grade << endl;
-    return 0;
+    }
+        return 0;
 }
