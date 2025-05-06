38ddebdd803a5a3eb8f84a9cb17677ec85b21537
Heet Mehta <heetmehta18125@gmail.com>
2025-05-06 15:45:18 +0530

    8.5 added

--- PRACTICAL FILES/8.4/8_4.cpp
+++ PRACTICAL FILES/8.4/8_4.cpp
@@ -7,6 +7,7 @@ int main() {
     vector<pair<int,string>> v;
     for (int i=0;i<n;++i) { string name; int score; cin>>name>>score; v.emplace_back(score,name); }
     sort(v.rbegin(), v.rend());
+    cout<<"-----------------------------------------------"<<endl;
     for (auto &p : v) cout << p.second << " " << p.first << endl;
     return 0;
 }
